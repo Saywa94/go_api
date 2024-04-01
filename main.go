d5bd7fa325a4f63c8259d9a8972a18a3f50a0390
Saywa94 <saywachavez@gmail.com>
2024-03-31 21:32:45 -0400

    Redis integration + graceful shutdown

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+
+	"os/signal"
 
 	"github.com/Saywa94/go_api/application"
 )
@@ -10,7 +13,10 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("Failed to start app: ", err)
 	}
