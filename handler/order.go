211c4a80bad8d9ae584d5f8b15cbbad91fb2600e
Saywa94 <saywachavez@gmail.com>
2024-04-01 22:29:10 -0400

    Added data model for Order

--- handler/order.go
+++ handler/order.go
@@ -7,22 +7,27 @@ import (
 
 type Order struct{}
 
+// Creates and order
 func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Create an order")
 }
 
+// List all orders
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("List all orders")
 }
 
+// Get an order by ID
 func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get an order by ID")
 }
 
+// Update an order by ID
 func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Update an order by ID")
 }
 
+// Delete and order by ID
 func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Delete and order by ID")
 }
