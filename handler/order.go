2adc4c9059810add1fab8bff7d835896cfeca029
Saywa94 <saywachavez@gmail.com>
2024-04-03 10:40:20 -0400

    Implemented ListAll orders to router handler

--- handler/order.go
+++ handler/order.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
@@ -62,7 +63,44 @@ func (h *Order) Create(w http.ResponseWriter, r *http.Request) {
 
 // List all orders
 func (h *Order) List(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("List all orders")
+	cursorStr := r.URL.Query().Get("cursor")
+	if cursorStr == "" {
+		cursorStr = "0"
+	}
+	const decimal = 10
+	const bitSize = 64
+	cursor, err := strconv.ParseUint(cursorStr, decimal, bitSize)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	const size = 50
+	res, err := h.Repo.FindAll(r.Context(), order.FindAllPage{Size: size, Offset: cursor})
+	if err != nil {
+		fmt.Println("Failed to finda all: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var response struct {
+		Items []model.Order `json:"items"`
+		Next  uint64        `json:"next,omitempty"`
+	}
+	response.Items = res.Orders
+	response.Next = res.Cursor
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("Failed to Marshall, ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(data)
+	w.WriteHeader(http.StatusOK)
+	return
+
 }
 
 // Get an order by ID
